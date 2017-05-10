65c02f563db571665ad3e172e33471db8b33fa3c
Paolo Pragides <ppragides@pragides.ca>
2017-05-10 11:21:18 -0400

    Updating docker run script

--- docker/pragides-run.sh
+++ docker/pragides-run.sh
@@ -65,8 +65,8 @@ done
 PROJECT_ROOT=$(pwd)
 GIT_URL="/home/ppragides/git"
 NODE_VERSION=v0.12.15
-PRAGIDES_DIR= pragides-static
-PRAGIDES_APP_DIR= pragides-app
+PRAGIDES_WEB_DIR=pragides-web
+PRAGIDES_APP_DIR=pragides-app
 
 # Blow away existing repos and start fresh
 if [[ $FRESH_CLONE ]]; then
@@ -75,8 +75,8 @@ if [[ $FRESH_CLONE ]]; then
 	ls -l
 fi
 
-if [ ! -e $PRAGIDES_DIR ]; then
-	git clone --recursive $GIT_URL/pragides-static.git $PRAGIDES_DIR
+if [ ! -e $PRAGIDES_WEB_DIR ]; then
+	git clone --recursive $GIT_URL/pragides-web.git $PRAGIDES_WEB_DIR
 fi
 
 if [ ! -e $PRAGIDES_APP ]; then
@@ -85,16 +85,16 @@ fi
 
 if [[ $GIT_PULL ]]; then
 	echo "Pulling changes down..."
-	cd $PROJECT_ROOT/$PRAGIDES_DIR; git pull $GIT_URL/pragides-static.git
+	cd $PROJECT_ROOT/$PRAGIDES_WEB_DIR; git pull $GIT_URL/pragides-web.git
 	cd $PROJECT_ROOT/$PRAGIDES_APP_DIR; git pull $GIT_URL/pragides-app.git
 fi
 
 
 # Install all the node-modules for shaw-music repo, if they do not exist
-#if [[ ! -e "$PROJECT_ROOT/$PRAGIDES_DIR/app/node_modules" || $NPM_INSTALL ]]; then
+#if [[ ! -e "$PROJECT_ROOT/$PRAGIDES_WEB_DIR/app/node_modules" || $NPM_INSTALL ]]; then
 #	echo Installing npm modules...
 #	docker run --rm \
-#		-v $PROJECT_ROOT/$PRAGIDES_DIR/app:/noderoot \
+#		-v $PROJECT_ROOT/$PRAGIDES_WEB_DIR/app:/noderoot \
 #		-e UID=1000 \
 #		-e GID=1000 \
 #		registry.labs.eng.shaw.ca/buildtools/npm:$NODE_VERSION
@@ -102,12 +102,10 @@ fi
 
 # NGINX container
 docker run -d \
-	-p 81:81 \
-	-p 444:444 \
+	-p 81:80 \
 	--restart=always \
-	--net container:pragides-local \
-	-v $PROJECT_ROOT/$PRAGIDES_DIR/webroot/:/var/www/mcs/shaw-music/webroot \
-	-v $PROJECT_ROOT/$PRAGIDES_DIR/etc/nginx/conf.d/:/etc/nginx/conf.d/ \
+	-v $PROJECT_ROOT/$PRAGIDES_WEB_DIR/webroot/:/var/www/pragides.ca/webroot \
+	-v $PROJECT_ROOT/$PRAGIDES_WEB_DIR/etc/nginx/conf.d/pragides.conf:/etc/nginx/nginx.conf \
 	-v /etc/localtime:/etc/localtime:ro \
 	--name pragides-static \
 	nginx:latest
@@ -115,7 +113,6 @@ docker run -d \
 # Consul DB container
 docker run -d \
 	-p 1440:1440 \
-	--net container:pragides-local \
 	-e CONSUL_BIND_INTERFACE=eth0 \
 	--name consul \
 	consul:latest \
